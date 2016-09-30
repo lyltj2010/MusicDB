f4adeebc2fc1cd978695f84916e059f7ea22a016
yongle <lyuyongle@gmail.com>
2016-09-30 00:35:39 -0500

    test form handling

---
+++ artist.php
@@ -0,0 +1,21 @@
+<?php
+include 'const.php';
+$conn = new mysqli($db_server, $db_user, $db_passwd);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+mysqli_select_db($conn,$db_name);
+
+$sql = "select name, url from artist limit {$_GET['number']}";
+$results = $conn->query($sql);
+
+if ($results->num_rows > 0) {
+	while($row = $results->fetch_assoc()){
+		echo $row['name'] . ' : ' . $row['url'];
+		echo "<br>";
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -2,26 +2,10 @@
 <html>
 <body>
 
-<?php
-include 'const.php';
-$conn = new mysqli($db_server, $db_user, $db_passwd);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+<form action="artist.php" method="GET">
+    Number:  <input type="number" name="number" />
+    <input type="submit" name="submit" value="Submit!" />
+</form>
 
-mysqli_select_db($conn,$db_name);
-$sql = "select name, url from artist limit 5";
-$results = $conn->query($sql);
-
-if ($results->num_rows > 0) {
-	while($row = $results->fetch_assoc()){
-		echo $row['name'] . ' : ' . $row['url'];
-		echo "<br>";
-	}
-} else {
-	echo "0 results";
-}
-$conn->close();
-?>
 </body>
 </html>

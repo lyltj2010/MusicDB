8e9a066d2eb95e618211751977f177d86459223c
yongle <lyuyongle@gmail.com>
2016-10-08 11:01:51 -0500

    comment part done

--- track.php
+++ track.php
@@ -10,15 +10,13 @@
   <?php include 'includes/header.php' ?>
   <?php include 'utils/track_util.php' ?>
   <div class="row">
-    <div class="col-md-1"></div>
-
-    <div class="col-md-10">
-      <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-3">
+        <div class="col-md-2 col-md-offset-2"></div>
+        <div class="col-md-2">
           <img src="<?=get_img($track['album'])['image']?>">
         </div>
-        <div class="col-md-7">
+
+        <div class="col-md-4">
+          <br>
           <h4><a href="<?=$track['url']?>"><?=$track['name']?></a></h4>
           <span><b>Artist:&nbsp;</b>
             <a href="artist.php?artist=<?=$track['artist']?>"><?=$track['artist']?></a>
@@ -29,29 +27,48 @@
           <span><b>Listeners:&nbsp;</b><?=$track['listeners']?></span><br>
           <span><b>Playcount:&nbsp;</b><?=$track['playcount']?></span><br>
         </div>
-      </div>
-    </div>
 
-    <div class="col-md-1"></div>
   </div>
 
-  <!-- comment -->
-  <!-- <div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
-      <div class="col-md-2"></div>
-      <div class="col-md-8">
-        <form>
-          <div class="form-group">
-            <label for="comment">Comment:</label>
-            <textarea class="form-control" rows="2" id="comment"></textarea>
-          </div>
-        </form>
-      </div> 
+  <div class="row">
+    <div class="col-md-1 col-md-offset-2"></div>
+    <div class="col-md-6">
+      <form action="" method="POST">
+        <div class="form-group">
+          <label for="comment">Comment:</label>
+          <textarea class="form-control" rows="2" name="comment"></textarea>
+          <input type="hidden" name="mbid_track" value="<?=$track['mbid_track']?>">
+          <input type="submit" name="submit" class="pull-right">
+        </div>
+      </form>
     </div>
-    <div class="col-md-1"></div>  
-  </div> -->
+  </div>
+
+  <div class="row">
+    <div class="col-md-1 col-md-offset-2"></div>
+    <div class="col-md-6">
+      <?php
+      if(isset($_POST['submit'])){
+        include 'resources/config.php';
+        $comment = $_POST['comment'];
+        $mbid_track = $_POST['mbid_track'];
+
+        $conn = mysqli_connect($db_server, $db_user, $db_passwd, $db_name);
+        $sql_insert = "INSERT INTO comment(content,track)
+                       VALUES (\"$comment\",\"$mbid_track\")";
+        mysqli_query($conn, $sql_insert);
+        mysqli_close($conn);
+      }
+      ?>
+      
+      <?php $comments=get_comments($track['mbid_track']) ?>
+      <?php foreach ($comments as $comment): ?>
+        <p><?=$comment['content']?> </p>
+      <?php endforeach ?>
+    </div>
+  </div>
+
 </div>
-<?php include 'includes/footer.php' ?>
 </body>
+
 </html>
--- utils/track_util.php
+++ utils/track_util.php
@@ -8,12 +8,21 @@ function get_img($album){
 	return $results;
 }
 
+function get_comments($mbid_track) {
+	// get comments of the given track
+	include 'resources/config.php';
+	$conn = mysqli_connect($db_server, $db_user, $db_passwd, $db_name);
+	$sql = "SELECT content FROM comment WHERE track=\"$mbid_track\"";
+	$results = mysqli_fetch_all(mysqli_query($conn,$sql),MYSQLI_ASSOC);
+	return $results;
+}
+
 include 'resources/config.php';
 $track_name = $_GET['track'];
 $conn = mysqli_connect($db_server, $db_user, $db_passwd, $db_name);
-$sql_track = "SELECT name, url, artist, album, listeners, playcount
+$sql_track = "SELECT name, url, mbid_track, artist, album, listeners, playcount
 			  FROM track WHERE name = \"$track_name\"";
 $track = mysqli_fetch_assoc(mysqli_query($conn, $sql_track));
 
-mysqli_close($conn)
+mysqli_close($conn);
 ?>
